b3d0634fbf7742c08438254412c505c5bd35b6a3
Heger Arias Santos <hegel.0218@gmail.com>
2022-06-18 21:26:47 -0400

    Queue changed to typescript

---
+++ dS/Queue/Single_Queue.ts
@@ -0,0 +1,69 @@
+import DoubleLinkedList from "../Linked_List/Double-LinkedList";
+
+class Queue {
+	length: number;
+	items: DoubleLinkedList;
+
+	constructor(data: string | number) {
+		this.length = 0;
+		this.items = new DoubleLinkedList(data);
+	}
+
+	/**
+	 * The function takes in a parameter of type string or number and adds it to the beginning of the
+	 * Queue
+	 * @param {string | number} data - The data to be added to the queue.
+	 */
+	enqueue(data: string | number) {
+		this.items.unshift(data);
+		this.length++;
+	}
+
+	/**
+	 * The dequeue() function removes the last element from the queue and returns it
+	 * @returns The last element in the queue.
+	 */
+	dequeue() {
+		let lastElement = this.items.tail.value;
+		this.items.pop();
+		this.length--;
+		return lastElement;
+	}
+
+	/**
+	 * The peek() function returns the value of the last item in the queue
+	 * @returns The value of the tail of the items Queue.
+	 */
+	peek() {
+		return this.items.tail.value;
+	}
+
+	/**
+	 * It returns the length of the Queue.
+	 * @returns The length of the Queue.
+	 */
+	size() {
+		return this.length;
+	}
+}
+
+/* fifo (First in First out)
+
+        -------------
+        4  3  2  1  0                
+        -------------
+      	
+
+*/
+
+let Queue2 = new Queue(0);
+
+Queue2.enqueue(1);
+Queue2.enqueue(2);
+Queue2.enqueue(3);
+Queue2.enqueue(4);
+Queue2.enqueue(5);
+
+while (Queue2.length !== 0) {
+	console.log(Queue2.dequeue());
+}
--- dS/Queue/readme.md
+++ dS/Queue/readme.md
@@ -2,21 +2,7 @@
 
 A Queue is a linear structure which follows a particular order in which the operations are performed. The order is First In First Out (FIFO). A good example of a queue is any queue of consumers for a resource where the consumer that came first is served first.
 
-## types
-
-- Simple Queue or Linear Queue
-- Priority Queue
-
 ## Simple Queue or Linear Queue
 
 In Linear Queue, an insertion takes place from one end while the deletion occurs from another end. The end at which the insertion takes place is known as the rear end, and the end at which the deletion takes place is known as front end. It strictly follows the FIFO rule.
 ![](../../img/DS/Queue.jpg)
-
-## Priority Queue
-
-It is a special type of queue in which the elements are arranged based on the priority. It is a special type of queue data structure in which every element has a priority associated with it. Suppose some elements occur with the same priority, they will be arranged according to the FIFO principle. The representation of priority queue is shown in the below image -
-
-![](../../img/DS/Priority_Queue.png)
-
-Types of Queue
-Insertion in priority queue takes place based on the arrival, while deletion in the priority queue occurs based on the priority. Priority queue is mainly used to implement the CPU scheduling algorithms.
