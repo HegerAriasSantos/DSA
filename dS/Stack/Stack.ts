4608064c434f84bfbf5b4f5c808a8a019ce4abda
Heger Arias Santos <hegel.0218@gmail.com>
2022-06-18 21:34:40 -0400

    documented

--- dS/Stack/Stack.ts
+++ dS/Stack/Stack.ts
@@ -8,6 +8,10 @@ export default class Stack {
 		this.items = new DoublyLinkedList(data);
 	}
 
+	/**
+	 * While the length of the stack is not zero, push the popped value to the array.
+	 * @returns An array of strings and numbers.
+	 */
 	Clear() {
 		let arr: Array<string | number> = [];
 		while (this.length !== 0) {
@@ -16,10 +20,18 @@ export default class Stack {
 		return arr;
 	}
 
+	/**
+	 * The Peek() function returns the value of first item in the stack.
+	 * @returns The value of the head node.
+	 */
 	Peek() {
 		return this.items.head.value;
 	}
 
+	/**
+	 * The function removes the first element from the stack and returns it.
+	 * @returns The first element of the list.
+	 */
 	Pop() {
 		this.length--;
 		let firstElement = this.items.head.value;
@@ -27,6 +39,10 @@ export default class Stack {
 		return firstElement;
 	}
 
+	/**
+	 * Push() adds a new item to the top of the stack
+	 * @param {string | number} data - The data to be added to the stack.
+	 */
 	Push(data: string | number) {
 		this.length++;
 		this.items.unshift(data);
